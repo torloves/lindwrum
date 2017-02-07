de31b4038341f627d49a9d7a7f6df5ceec29a0f2
CyberNeet <saborn3116@gmail.com>
2017-02-07 15:55:57 +0900

    move
    キー入力でPlayerが動くスクリプトを追加しました。

---
+++ Assets/scripts/playerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+
+    int x,y;
+
+	// Use this for initialization
+	void Start () {
+        x=0;
+        y=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x--;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x++;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            y++;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            y--;
+        }
+        this.transform.Translate(x, y, 0);
+        x = 0;
+        y = 0;
+	}
+}
